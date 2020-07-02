e79306dc6855a4850a5a4c1219c063759df01c4f
Paul Onteri <onteripaul@gmail.com>
2020-07-02 11:25:41 +0300

    Rewrite singly linked list

--- LinkedLists/linked_list.py
+++ LinkedLists/linked_list.py
@@ -4,166 +4,165 @@ The first node is called the head, and it’s used as the starting point for any
 The last node must have its next reference pointing to None to determine the end of the list.
 """
 
-class Node(object):
-    def __init__(self,value):
+
+class Node:
+    def __init__(self, value):
         self.value = value
         self.next = None
 
-class LinkedList(object):
+
+class LinkedList:
     def __init__(self, head=None):
-        self.head = head
-    
-    def append(self, new_element):
-        current =  self.head
-        if self.head:
-            # iterate through the next reference in every Node until you reach the end of the list
-            while current.next:
-                current = current.next
-            # set next for the end of the list to be the new_element 
-            current.next = new_element
-        # if there is no head, you should just assign new_element to it
-        else:
-            self.head = new_element
-            
-            
-    # deletes by value(not 'index')        
-    def delete(self, value):
+        self.head = None
+
+    def print_list(self):
         current = self.head
-        previous = None
-        while current.value != value and current.next:
-            previous = current
+        values = []
+        while current:
+            values.append(current.value)
             current = current.next
-        if current.value == value:
-            if previous:
-                previous.next = current.next
-            else:
-                self.head = current.next
-                
-    def get_position(self, position):
-        counter = 0
-        current = self.head
-        if position < 0:
-            return None
-        while current and counter <= position:
-            if counter == position:
-                return current
-            current = current.next
-            counter += 1
-        return None
-    
-    def insert(self, new_element, position):
-        counter = 0
-        current = self.head
-        if position == 0:
-            new_element.next = self.head
-            self.head = new_element
-        elif position > 0:
-            while current and counter < position:
-                if counter ==  position - 1:
-                    new_element.next = current.next 
-                    current.next =  new_element
-                #
-                current = current.next
-                counter += 1
-    
-    # Returns the length of the linked list
+        print(values)
+
     def length(self):
+        # length of list
         counter = 0
         current = self.head
         while current:
             counter += 1
             current = current.next
         return counter
-            
-    # Prints out the linked list in traditional Python list format
-    def display(self):
-        elements = []
+
+    def append(self, value):
+        # add value to the end of the list
+        new_node = Node(value)
         current = self.head
-        while current:
-            elements.append(current.value)
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_node
+        else:
+            self.head = new_node
+
+    def prepend(self, value):
+        # add value to the beggining of the list
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete_by_value(self, value):
+        # delete by value
+        if not self.head:
+            raise Exception(
+                "ERROR: Cannot delete element in an empty Linked List!")
+        current = self.head
+        prev = None
+        while current.value != value and current.next:
+            prev = current
             current = current.next
-        print(elements)
-        
-    # Deletes the node at index 'position'
-    def erase(self, position):
-        if position > self.length():
-            return print("ERROR: Index out of range!")
-        
-        if self.head == None: 
-            return
-        
-        counter =  0
-        current =  self.head
-        
-        if position == 0:
+        if current.value == value:
+            if prev:
+                prev.next = current.next
+            else:
+                self.head = self.head.next
+
+    def delete_by_index(self, index):
+        # delete by index
+        if index > self.length() - 1:
+            raise Exception("ERROR: Index out of range!")
+
+        counter = 0
+        current = self.head
+        prev = None
+
+        if index == 0:
             self.head = current.next
             return
-            
 
-        while current.next and counter < position:
-            previous = current
+        while current.next and index > counter:
+            prev = current
             current = current.next
-            if counter == position - 1:
-                previous.next = current.next              
+            if counter == index - 1:
+                prev.next = current.next
             counter += 1
 
-"""
-Testing
-"""
+    def insert(self, value, index):
+        # insert new node at index
+        new_node = Node(value)
 
-# Set up some Nodes
-e1 = Node(1)
-e2 = Node(2)
-e3 = Node(3)
-e4 = Node(4)
-e5 = Node(5)
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+        elif index > 0:
+            counter = 0
+            current = self.head
 
-# Start setting up a LinkedList
-ll = LinkedList(e1)
-ll.append(e2)
-ll.append(e3)
-ll.append(e5)
+            while current.next and counter < index:
+                if index - 1 == counter:
+                    new_node.next = current.next
+                    current.next = new_node
+                current = current.next
+                counter += 1
 
-# Test Display
-print("\nTest Display")
-ll.display()
+    def get_index(self, index):
+        # get node at index
+        if index < 0 or index > self.length() - 1:
+            return None
 
-# Test Length 
-print("\nTest Length")
-print("length is equal to: " + str(ll.length()))
+        if index == 0:
+            return self.head
 
-# Test get_position()
-print("\nTest get_position()")
-# Should print 3
-print(ll.head.next.next.value)
-# Should also print 3
-print(ll.get_position(2).value)
+        counter = 0
+        current = self.head
+        while current and counter <= index:
+            if index == counter:
+                return current
+            current = current.next
+            counter += 1
 
-
-# Test Insert
-print("\nTest Insert")
-ll.insert(e4,2)
-# should print 4 now
-print("Should print 4 now")
-print(ll.get_position(2).value)
+        return None
 
 
-# Test Delete
-print("\nTest Delete")
-ll.display()
-one = ll.get_position(0).value
-two = ll.get_position(1).value
-three = ll.get_position(2).value
-print("Deleting...")
-ll.delete(one)
-print("Should print: " + str(two))
-print(ll.get_position(0).value)
-print("Should print: " + str(three))
-print(ll.get_position(1).value)
-ll.display()
+# TESTING
+q = LinkedList()
 
-# Test Erase
-print("\nTest Erase")
-ll.display()
-ll.erase(1)
-ll.display()
+q.append(1)
+q.append(2)
+q.append(3)
+q.append(4)
+q.append(5)
+q.append(6)
+print("Test Append. Should print: [1, 2, 3, 4, 5, 6]")
+print("Test Append:")
+q.print_list()
+
+#
+print("Length:" + str(q.length()))
+
+#
+q.prepend(0)
+print("Test prepend. Should print: [0, 1, 2, 3, 4, 5, 6]")
+print("Test prepend:")
+q.print_list()
+
+#
+q.delete_by_value(0)
+print("Test delete_by_value. Should print: [1, 2, 3, 4, 5, 6]")
+print("Test delete_by_value:")
+q.print_list()
+
+#
+q.delete_by_index(1)
+print("Test delete_by_index. Should print: [1, 3, 4, 5, 6]")
+print("Test delete_by_index:")
+q.print_list()
+
+#
+print("Test get_index. Should print:4")
+print("Test get_index: " + str(q.get_index(2).value))
+q.print_list()
+
+#
+q.insert(2, 1)
+print("Test delete_by_index. Should print: [1, 2, 3, 4, 5, 6]")
+print("Test delete_by_index:")
+q.print_list()
